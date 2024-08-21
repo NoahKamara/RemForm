12af2372e3ca5c71f574f166bec644b6bc8787d4
Noah Kamara <mail@noahkamara.com>
2024-08-21 02:17:14 +0200

    feat: working prototype

--- Sources/Interface/SchemaView.swift
+++ Sources/Interface/SchemaView.swift
@@ -8,9 +8,9 @@ public struct SchemaView: View {
     @Bindable
     var bindings: FormBindings
 
-    public init(schema: FormSchema, values: FormValues? = nil) {
+    public init(schema: FormSchema, bindings: FormBindings? = nil) {
         self.schema = schema
-        self.bindings = FormBindings(initialValues: values)
+        self.bindings = bindings ?? .init()
     }
 
     typealias Element = (offset: Int, element: FormSchema.Summary.Word)
@@ -20,31 +20,32 @@ public struct SchemaView: View {
     }
 
     public var body: some View {
-        VStack {
-            Naturals {
-                ForEach(params, id:\Element.offset) { element in
-                    switch element.element {
-                    case .word(let word): Text(word)
-                    case .parameter(let paramID):
-                        if let parameter = schema.parameters.first(where: { $0.id == paramID }) {
-                            ParameterView(
-                                parameter: parameter,
-                                value: $bindings[paramID]
-                            )
-                        } else {
-                            Text("{\(paramID)}")
-                                .foregroundStyle(.red)
-                        }
+        Naturals {
+            ForEach(params, id:\Element.offset) { element in
+                switch element.element {
+                case .word(let word): Text(word)
+                case .parameter(let paramID):
+                    if let parameter = schema.parameters.first(where: { $0.id == paramID }) {
+                        ParameterView(
+                            parameter: parameter,
+                            value: $bindings[paramID]
+                        )
+                    } else {
+                        Text("{\(paramID)}")
+                            .foregroundStyle(.red)
                     }
                 }
             }
-
         }
     }
 }
 
 
 #Preview("Names Form") {
+    @Previewable @Bindable var bindings = FormBindings(initialValues: .init(values: [
+        "first_name": .primitive(.string("John"))
+    ]))
+
     let schema = FormSchema(
         parameters: [
             .init(id: "last_name", type: .primitive(.string)),
@@ -53,20 +54,27 @@ public struct SchemaView: View {
         configuration: .summary("Enter your \("last_name") and \("first_name") to get a price.")
     )
 
-    SchemaView(schema: schema)
+    SchemaView(schema: schema, bindings: bindings)
 }
 
 
 #Preview("Debug") {
+    @Previewable @Bindable var bindings = FormBindings(initialValues: .init(values: [
+        "first_name": .primitive(.string("John"))
+    ]))
+
     let schema = FormSchema(
         parameters: [
+            .init(id: "first_name", type: .primitive(.string)),
             .init(id: "last_name", type: .primitive(.string)),
-            .init(id: "first_name", type: .primitive(.string))
         ],
-        configuration: .summary("Enter your \("last_name") and \("first_name") to get a price.")
+        configuration: .summary("Enter your \("first_name") and \("last_name") to get a price.")
     )
 
-    SchemaView(schema: schema)
+    VStack {
+        SchemaView(schema: schema, bindings: bindings)
+        FormValuesDebugView(schema: schema, values: bindings.values)
+    }
 }
 
 
